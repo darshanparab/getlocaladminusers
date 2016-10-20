71a684e92f2f28a4389612f9a3c60667e3a50588
darshanparab <mr.darshanparab@gmail.com>
2016-10-20 19:07:38 +0530

    Minimum Acceptable Solution
    Code fetches only the user accounts present in administrator group and exports them to CSV

--- main.cpp
+++ main.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
 #include <windows.h>
 #include <lm.h>
+#include <fstream>
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv)
 {
-	wchar_t LOCALGRP[100]= L"Administrators";
-	DWORD LEVEL=1;
-	LPBYTE bufptr,*USERINFO;
-	DWORD prefmaxlength = MAX_PREFERRED_LENGTH;
-	DWORD entriesread;
-	DWORD totalentries;
-	int i=0;
-	
+	wchar_t LOCALGRP[100]= L"Administrators", HOSTNAME[MAX_COMPUTERNAME_LENGTH+1], PWD[MAX_PATH+1];
+	DWORD bufSize = MAX_COMPUTERNAME_LENGTH;
+	DWORD LEVEL=1, prefmaxlength=MAX_PREFERRED_LENGTH, entriesread, totalentries;
+	PLOCALGROUP_MEMBERS_INFO_1 bufptr;
+	FILE *RESULTFILE;
 	system("cls");
-	NetLocalGroupGetMembers(NULL,LOCALGRP,LEVEL,&bufptr,prefmaxlength,&entriesread,&totalentries,0);
-	cout<<entriesread<<" "<<totalentries<<endl;
+			
+	GetCurrentDirectoryW(MAX_PATH,PWD);
+	GetComputerNameW(HOSTNAME,&bufSize);
+	wcscat(PWD,L"\\");
+	wcscat(PWD,HOSTNAME);
+	wcscat(PWD,L".csv");
+	RESULTFILE = _wfopen(PWD,L"w");
+
+	fputws(L"\"Hostname\",\"Username\"\n",RESULTFILE);
+	if(NetLocalGroupGetMembers(NULL,LOCALGRP,LEVEL,(LPBYTE *)&bufptr,prefmaxlength,&entriesread,&totalentries,0) == NERR_Success)
+	{
+		for(int i=0; i<entriesread; i++)
+		{
+			if(bufptr[i].lgrmi1_sidusage == SidTypeUser)
+			{
+				fputws(L"\"",RESULTFILE);
+				fputws(HOSTNAME,RESULTFILE);
+				fputws(L"\",\"",RESULTFILE);
+				fputws(bufptr[i].lgrmi1_name,RESULTFILE);
+				fputws(L"\"\n",RESULTFILE);
+			}
+		}
+	}
+	else
+	{
+	}
 	
-	LOCALGROUP_MEMBERS_INFO_1 *USER = new LOCALGROUP_MEMBERS_INFO_1[entriesread];
-	USER=&(LOCALGROUP_MEMBERS_INFO_1 &)bufptr;
-	cout<<sizeof(USER)<<endl;
-		
-	delete[] USER;
 	NetApiBufferFree(&bufptr);
-		
+	fclose(RESULTFILE);
 //	system("PAUSE");
+	
 	return 0;
 }
