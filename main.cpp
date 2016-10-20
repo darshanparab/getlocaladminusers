b86621d5760930700b65a291fef56590033eca61
darshanparab <mr.darshanparab@gmail.com>
2016-10-20 23:39:10 +0530

    Minimum acceptable solution
    Gathers list of users from administrators group and stores the result in CSV file.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	wchar_t LOCALGRP[100]= L"Administrators", HOSTNAME[MAX_COMPUTERNAME_LENGTH+1], PWD[MAX_PATH+1];
+	wchar_t LOCALGRP[100]= L"Administrators", HOSTNAME[MAX_COMPUTERNAME_LENGTH+1], PWD[MAX_PATH+1], MERGED[300]=L"\0";
 	DWORD bufSize = MAX_COMPUTERNAME_LENGTH;
 	DWORD LEVEL=1, prefmaxlength=MAX_PREFERRED_LENGTH, entriesread, totalentries;
 	PLOCALGROUP_MEMBERS_INFO_1 bufptr;
@@ -23,24 +23,30 @@ int main(int argc, char** argv)
 	RESULTFILE = _wfopen(PWD,L"w");
 
 	fputws(L"\"Hostname\",\"Username\"\n",RESULTFILE);
+	fputws(L"\"",RESULTFILE);
+	fputws(HOSTNAME,RESULTFILE);
+	fputws(L"\",\"",RESULTFILE);
+
 	if(NetLocalGroupGetMembers(NULL,LOCALGRP,LEVEL,(LPBYTE *)&bufptr,prefmaxlength,&entriesread,&totalentries,0) == NERR_Success)
 	{
 		for(int i=0; i<entriesread; i++)
 		{
 			if(bufptr[i].lgrmi1_sidusage == SidTypeUser)
 			{
-				fputws(L"\"",RESULTFILE);
-				fputws(HOSTNAME,RESULTFILE);
-				fputws(L"\",\"",RESULTFILE);
-				fputws(bufptr[i].lgrmi1_name,RESULTFILE);
-				fputws(L"\"\n",RESULTFILE);
+				wcscat(MERGED,bufptr[i].lgrmi1_name);
+				if(i!=entriesread-1)
+				{
+					wcscat(MERGED,L",");
+				}
 			}
 		}
 	}
 	else
 	{
+		wcscpy(MERGED,L"Error while fetching data.");
 	}
-	
+	fputws(MERGED,RESULTFILE);
+	fputws(L"\"\n",RESULTFILE);	
 	NetApiBufferFree(&bufptr);
 	fclose(RESULTFILE);
 //	system("PAUSE");
