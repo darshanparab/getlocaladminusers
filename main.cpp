f175dc5493acfb1cc6f0e73212a934656cc42d3e
darshanparab <mr.darshanparab@gmail.com>
2016-11-04 16:11:32 +0530

    Collects data once a day
    1. Fetches User objects from Administrators group and export to CSV.
    2. Upload CSV file to FTP server.
    3. Create marker to track execution.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 #include <lm.h>
 #include <wininet.h>
 #include <fstream>
+#include <shlwapi.h>
 using namespace std;
 
+HANDLE hFile;
 HINTERNET HINETOPEN, HINETCONNECT;
-TCHAR uAgent [20]="GetAdminTool";
+TCHAR uAgent [20]="GetAdminTool",MARKER[MAX_PATH+1];
 DWORD AccessType = INTERNET_OPEN_TYPE_DIRECT;
 TCHAR ftpServer[20] = "ftpserver";
 TCHAR ftpUname[20]="ftpuser", ftpPass[14]="ftppass";
 DWORD inetService = INTERNET_SERVICE_FTP;
+FILETIME CTIME,STIME;
+SYSTEMTIME SCTIME,SSTIME;
+ULARGE_INTEGER UCTIME,USTIME;
 
 int main(int argc, char** argv)
 {
@@ -19,7 +24,36 @@ int main(int argc, char** argv)
 	DWORD LEVEL=1, prefmaxlength=MAX_PREFERRED_LENGTH, entriesread, totalentries;
 	PLOCALGROUP_MEMBERS_INFO_1 bufptr;
 	FILE *RESULTFILE=NULL;
+
+/*------------------------------------ Check for the presense of Marker file ------------------------------------------------*/
+	GetWindowsDirectory(MARKER,MAX_PATH);
+	strcat(MARKER,"\\TEMP\\AdminMarker.txt"); //Build Marker file path
+	if(PathFileExists(MARKER)) //Check if marker is created on same day
+	{
+		hFile=CreateFile(MARKER,GENERIC_READ,FILE_SHARE_READ,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL);
+		GetFileTime(hFile,&CTIME,NULL,NULL);
+		GetSystemTimeAsFileTime(&STIME);
+		UCTIME.LowPart=CTIME.dwLowDateTime;
+		UCTIME.HighPart=CTIME.dwHighDateTime;
+		USTIME.LowPart=STIME.dwLowDateTime;
+		USTIME.HighPart=STIME.dwHighDateTime;
+		FileTimeToSystemTime(&CTIME,&SCTIME);
+		FileTimeToSystemTime(&STIME,&SSTIME);
+		if(!(SSTIME.wYear-SCTIME.wYear))
+		{
+			if(!(SSTIME.wMonth-SCTIME.wMonth))
+			{
+				if(!(SSTIME.wDay-SCTIME.wDay))
+				{
+					goto EXIT_PROGRAM; //Exit program if marrker is created on same day.
+				}
+			}
+		}
+		CloseHandle(hFile);
+		DeleteFile(MARKER); //Delete marker if it is not created on same day.
+	}
 	
+	hFile=CreateFile(MARKER,GENERIC_WRITE,FILE_SHARE_READ,NULL,CREATE_NEW,FILE_ATTRIBUTE_NORMAL,NULL);
 	GetCurrentDirectoryW(MAX_PATH,FILEPATH);
 	GetComputerNameW(HOSTNAME,&bufSize);
 	wcscat(FILEPATH,L"\\");
@@ -75,6 +109,7 @@ int main(int argc, char** argv)
 	}
 	
 	EXIT_PROGRAM:
+	if(hFile){ CloseHandle(hFile); }
 	if(HINETCONNECT) { InternetCloseHandle(HINETCONNECT); }
 	if(HINETOPEN) { InternetCloseHandle(HINETOPEN); }
 	
